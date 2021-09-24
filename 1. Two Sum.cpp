3ffc3074eb5e76e179d0f0c459382928d2af3467
mBlumTechnion <82164285+mBlumTechnion@users.noreply.github.com>
2021-09-24 23:59:23 +0300

    Update 1. Two Sum.cpp

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -1,15 +1,33 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> hashmap;
-        vector<int> outputVec;
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        std::unordered_map<int, int> hashmap;
+        std::vector<int> outputVec;
         for (int i=0; i<nums.size(); i++){
-            hashmap.insert(make_pair(nums[i],i));
+            hashmap.insert(std::make_pair(nums[i],i));
         }
+        int count =0;
         for (int num : nums){
-            if (hashmap.count(target - num)>0){
+            if (target-num == num){
+                if (count>0){
+                    outputVec.push_back(hashmap.find(target-num)->second); 
+                }
+                else{
+                    for (int j=0; j<nums.size(); j++){
+                        if (num == nums[j]){
+                            count++;
+                            if (count > 1){
+                                outputVec.push_back(j);
+                            }
+                        }
+                    }
+                }
+            }
+            else if (hashmap.count(target - num)>0){
                 outputVec.push_back(hashmap.find(num)->second);
-                outputVec.push_back(hashmap.find(target-num)->second);
+                if (outputVec.size()<2){
+                    outputVec.push_back(hashmap.find(target-num)->second);
+                }
                 return outputVec;
             }
         }
